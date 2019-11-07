9803bf34eefccf1a953bf114750012fd2059ea0f
Andrey74Orlov <orlov174andrey@mail.ru>
2019-11-07 17:00:17 +0500

    Updates

---
+++ precache-manifest.effb1b36340026fa38d70637198d2e32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c4129c00b78146e9c12a98450392d7",
+    "url": "/demo_social_network/index.html"
+  },
+  {
+    "revision": "6a20918b9b19cca0bd9e",
+    "url": "/demo_social_network/static/css/3.2763f5ba.chunk.css"
+  },
+  {
+    "revision": "d67602bcbba91027907b",
+    "url": "/demo_social_network/static/css/main.ae9f707d.chunk.css"
+  },
+  {
+    "revision": "acfd570086a76f29240c",
+    "url": "/demo_social_network/static/js/2.486858b4.chunk.js"
+  },
+  {
+    "revision": "6a20918b9b19cca0bd9e",
+    "url": "/demo_social_network/static/js/3.5cf37d7d.chunk.js"
+  },
+  {
+    "revision": "d67602bcbba91027907b",
+    "url": "/demo_social_network/static/js/main.14de9920.chunk.js"
+  },
+  {
+    "revision": "00b6d93ac0f1cf58554c",
+    "url": "/demo_social_network/static/js/runtime~main.764a7651.js"
+  },
+  {
+    "revision": "e817279537a0417d042f62fbb1b99eea",
+    "url": "/demo_social_network/static/media/preloader.e8172795.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo_social_network/precache-manifest.9161070b15a7603ebdbd5ad269223318.js"
+  "/demo_social_network/precache-manifest.effb1b36340026fa38d70637198d2e32.js"
 );
 
 self.addEventListener('message', (event) => {
